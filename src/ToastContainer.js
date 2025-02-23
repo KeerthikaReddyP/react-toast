6e22eb589d10a9fcea074143af6d1751070544d5
Keerthika Reddy <keerthikareddy267@gmail.com>
2025-02-23 21:42:56 +0530

    Success Toast styling

--- src/ToastContainer.js
+++ src/ToastContainer.js
@@ -1,7 +1,9 @@
 const ToastContainer = () => {
   return (
     <div>
-        
+        <div className="fixed right-5 top-5">
+          <div className="p-2 bg-green-400 rounded-md w-60 flex justify-between">Success message <span className="cursor-pointer">x</span></div>
+        </div>
     <div className="m-2 p-2 flex justify-center items-center h-screen">
       <button className="m-2 p-1 border-2 border-gray-500 rounded-md">
         Success toast
