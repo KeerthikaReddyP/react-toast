ca7cb6f0fa388158a7afc090202b162e0a3f22a3
Keerthika Reddy <keerthikareddy267@gmail.com>
2025-02-24 19:41:10 +0530

    Coloring the toasts based on type

--- src/ToastContainer.js
+++ src/ToastContainer.js
@@ -12,13 +12,20 @@ const ToastContainer = () => {
     setToasts(newToasts);
   };
 
+  const toastStyles={
+    "success": "bg-green-700",
+    "error":"bg-red-600",
+    "info":"bg-blue-600",
+    "warning":"bg-yellow-500"
+  }
+
   return (
     <div>
       <div className="fixed right-5 top-5">
         {toasts.map(({ id, message, type }) => (
           <div
             key={id}
-            className="px-4 py-2 m-2 bg-green-700 text-white rounded-md w-60 flex justify-between animate-slide"
+            className={`px-4 py-2 m-2 bg-green-700 text-white rounded-md w-60 flex justify-between animate-slide ${toastStyles[type]}`}
           >
             {message}
             <span className="cursor-pointer text-lg" onClick={handleClose}>
