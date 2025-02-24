2f8e084230cd9ccab73856bcb8928464ebb0dcfc
Keerthika Reddy <keerthikareddy267@gmail.com>
2025-02-24 18:37:20 +0530

    Display toast on button click

--- src/ToastContainer.js
+++ src/ToastContainer.js
@@ -1,15 +1,20 @@
-import './index.css';
+import { useState } from "react";
+import "./index.css";
 
 const ToastContainer = () => {
+  const [show, setShow] = useState(false);
+
   return (
     <div>
       <div className="fixed right-5 top-5">
-        <div className="px-4 py-2 bg-green-700 text-white rounded-md w-60 flex justify-between animate-slide">
-          Success message <span className="cursor-pointer text-lg">x</span>
-        </div>
+        {show && (
+          <div className="px-4 py-2 bg-green-700 text-white rounded-md w-60 flex justify-between animate-slide">
+            Success message <span className="cursor-pointer text-lg">x</span>
+          </div>
+        )}
       </div>
       <div className="m-2 p-2 flex justify-center items-center h-screen">
-        <button className="m-2 p-1 border-2 border-gray-500 rounded-md">
+        <button className="m-2 p-1 border-2 border-gray-500 rounded-md" onClick={()=>setShow(true)}>
           Success toast
         </button>
         <button className="m-2 p-1 border-2 border-gray-500 rounded-md">
