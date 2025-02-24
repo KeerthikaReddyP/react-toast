cf83c0a0e12d47275e2bc7f01c3b3e136aa18238
Keerthika Reddy <keerthikareddy267@gmail.com>
2025-02-24 10:28:29 +0530

    Some styling

--- src/ToastContainer.js
+++ src/ToastContainer.js
@@ -4,8 +4,8 @@ const ToastContainer = () => {
   return (
     <div>
       <div className="fixed right-5 top-5">
-        <div className="p-2 bg-green-400 rounded-md w-60 flex justify-between animate-slide">
-          Success message <span className="cursor-pointer">x</span>
+        <div className="px-4 py-2 bg-green-700 text-white rounded-md w-60 flex justify-between animate-slide">
+          Success message <span className="cursor-pointer text-lg">x</span>
         </div>
       </div>
       <div className="m-2 p-2 flex justify-center items-center h-screen">
