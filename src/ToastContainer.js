4e5b3e18a5b929a2d7f3601ec6d07bd88ae8c8bf
Keerthika Reddy <keerthikareddy267@gmail.com>
2025-02-24 19:21:10 +0530

    Multiple toast messages

--- src/ToastContainer.js
+++ src/ToastContainer.js
@@ -2,28 +2,36 @@ import { useState } from "react";
 import "./index.css";
 
 const ToastContainer = () => {
-  const [show, setShow] = useState(false);
+  const [toasts, setToasts] = useState([]);
 
-  const handleClose=()=>{
-    setShow(false);
-  }
+  const handleClose = () => {};
 
-  const handleAdd=()=>{
-    setShow(true);
-    setTimeout(handleClose,3000);
-  }
+  const handleAdd = (message, type) => {
+    const id = new Date().getTime();
+    const newToasts = [...toasts, { id, message, type }];
+    setToasts(newToasts);
+  };
 
   return (
     <div>
       <div className="fixed right-5 top-5">
-        {show && (
-          <div className="px-4 py-2 bg-green-700 text-white rounded-md w-60 flex justify-between animate-slide">
-            Success message <span className="cursor-pointer text-lg" onClick={handleClose}>x</span>
+        {toasts.map(({id,message,type}) => (
+          <div
+            key={id}
+            className="px-4 py-2 bg-green-700 text-white rounded-md w-60 flex justify-between animate-slide"
+          >
+            {message}
+            <span className="cursor-pointer text-lg" onClick={handleClose}>
+              x
+            </span>
           </div>
-        )}
+        ))}
       </div>
       <div className="m-2 p-2 flex justify-center items-center h-screen">
-        <button className="m-2 p-1 border-2 border-gray-500 rounded-md" onClick={handleAdd}>
+        <button
+          className="m-2 p-1 border-2 border-gray-500 rounded-md"
+          onClick={()=>handleAdd("Success toast", "success")}
+        >
           Success toast
         </button>
         <button className="m-2 p-1 border-2 border-gray-500 rounded-md">
